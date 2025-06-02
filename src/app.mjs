bcecb8d593acf9ee41c4dfc59730613817f21695
MatildaSundqvist <matildasundqvist@live.se>
2025-06-02 10:00:29 +0200

    added middleware errorHandler

--- src/app.mjs
+++ src/app.mjs
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import errorHandler from './middleware/errorHandler.mjs';
 
 dotenv.config({ path: '/config/config.env' });
 
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandler)
 
 export { app };
